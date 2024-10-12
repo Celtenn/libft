45cf8353cbe211afab21693714270d5217be2aa9
Celtenn <enescelten@gmail.com>
2024-10-13 01:18:23 +0300

    Add files via upload

---
+++ ft_putstr_fd.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    int i;
+
+    i = 0;
+    if(!s)
+        return ;
+    while(s[i] != '\0')
+    {
+        i++;
+    }
+    write(fd, s, i);
+}
