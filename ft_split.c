b212777218ec480940947f647e6b0742e63f9180
Idris Enes Kahraman <idkahram@k2m16s05.42kocaeli.com.tr>
2024-10-19 04:39:44 +0300

    libft

--- ft_split.c
+++ ft_split.c
@@ -1,100 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idkahram <idkahram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 03:00:11 by idkahram          #+#    #+#             */
+/*   Updated: 2024/10/19 04:27:50 by idkahram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
-#include <stdio.h>
 
-int countw(char const *s, char c)
+int	countw(char const *s, char c)
 {
-    int i;
-    int wnumber;
+	int	i;
+	int	wnumber;
 
-    i = 0;
-    wnumber = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-        {
-            i++;
-        }
-        else
-        {
-            wnumber++;
-            while (s[i] != c && s[i])
-            {
-                i++;
-            }
-        }
-    }
-    return (wnumber);
+	i = 0;
+	wnumber = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			wnumber++;
+			while (s[i] != c && s[i])
+			{
+				i++;
+			}
+		}
+	}
+	return (wnumber);
 }
 
-char    *creatw(char *daz, char const *s, int first_i, int last_i)
+char	*creatw(char *daz, char const *s, int first_i, int last_i)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < last_i)
-    {
-        daz[i] = s[first_i + i];
-        i++;
-    }
-    daz[i] = '\0';
-    return(daz);
+	i = 0;
+	while (i < last_i)
+	{
+		daz[i] = s[first_i + i];
+		i++;
+	}
+	daz[i] = '\0';
+	return (daz);
 }
 
-char    **create_word(char **daza, char const *s, char c, int k_len)
+char	*create_single_word(char const *s, int *index, char c)
 {
-    int i;
-    int k;
-    int w_len;
-    int first;
+	int		w_len;
+	int		first;
+	char	*word;
 
-    i = 0;
-    k = 0;
-    while (s[i] && k < k_len)
-    {
-        if (s[i] != c)
-        {
-            w_len = 0;
-            first = i;
-            while (s[i] != c && s[i])
-            {
-                w_len++;
-                i++;
-            }
-            daza[k] = (char *)malloc(sizeof(char) * (w_len + 1));
-            if (!daza[k])
-            {
-                while(k > -1)
-                {
-                    free(daza[k]);
-                    k--;
-                }
-                free(daza);
-                return(0);
-            }
-            creatw(daza[k], s, first, w_len);
-            k++;
-        }
-        else
-            i++;
-    }
-    daza[k] = NULL;
-    return(daza);
+	w_len = 0;
+	first = *index;
+	while (s[*index] != c && s[*index])
+	{
+		w_len++;
+		(*index)++;
+	}
+	word = (char *)malloc(sizeof(char) * (w_len + 1));
+	if (!word)
+		return (0);
+	creatw(word, s, first, w_len);
+	return (word);
 }
 
-char **ft_split(char const *s, char c)
+char	**create_word(char **daza, char const *s, char c, int k_len)
 {
-    int i;
-    int k_len;
-    char **dizi;
+	int	i;
+	int	k;
 
-    i = 0;
-    if (!s)
-        return(0);
-    k_len = countw(s, c);
-    dizi = (char **)malloc(sizeof(char *) * (k_len + 1));
-    if (!dizi)
-        return(0);
-    dizi = create_word(dizi, s, c, k_len);
-    return(dizi);
+	i = 0;
+	k = 0;
+	while (s[i] && k < k_len)
+	{
+		if (s[i] != c)
+		{
+			daza[k] = create_single_word(s, &i, c);
+			if (!daza[k])
+			{
+				while (--k > -1)
+					free(daza[k]);
+				free(daza);
+				return (0);
+			}
+			k++;
+		}
+		else
+			i++;
+	}
+	daza[k] = NULL;
+	return (daza);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		k_len;
+	char	**dizi;
+
+	i = 0;
+	if (!s)
+		return (0);
+	k_len = countw(s, c);
+	dizi = (char **)malloc(sizeof(char *) * (k_len + 1));
+	if (!dizi)
+		return (0);
+	dizi = create_word(dizi, s, c, k_len);
+	return (dizi);
 }
